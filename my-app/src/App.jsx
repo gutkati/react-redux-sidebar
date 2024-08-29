ff64fcd7ecc5e4e51cfb324f9d7fc3ed9e47ffd2
Ekaterina Shmeleva <gutkati@mail.ru>
2024-08-29 10:16:37 +0400

    Установка и подготовка React-приложения

---
+++ my-app/src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const App = () => {
+    return <h2>This is my Redux app!</h2>
+};
+
+export default App;
