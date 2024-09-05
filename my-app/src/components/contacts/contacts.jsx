cf0bc82d41a94b801072f7688785ba1f8079aaa2
Ekaterina Shmeleva <gutkati@mail.ru>
2024-09-05 10:46:43 +0400

    Редактировать таблицу

--- my-app/src/components/contacts/contacts.jsx
+++ my-app/src/components/contacts/contacts.jsx
@@ -4,31 +4,34 @@ import styles from './Contacts.module.css'
 const Contacts = () => {
     return (
         <div className={styles.contacts}>
-            <thead>
+            <h2 className={styles.title}>Семья</h2>
+
             <table className={styles.table}>
+                <thead>
                 <tr>
                     <th className={styles.header}>Имя</th>
                     <th className={styles.header}>Описание</th>
                     <th className={styles.header}>Контакты</th>
                 </tr>
+                </thead>
 
+                <tbody>
                 <tr className={styles.cell}>
                     <td>Катя</td>
                     <td>Сестра</td>
                     <td>8-000-200-11-00</td>
-                    <td/>
-                    <td/>
+                    <td className={styles.btn__style}/>
+                    <td className={styles.btn__style}/>
                 </tr>
                 <tr className={styles.cell}>
                     <td>Женя</td>
                     <td>Брат</td>
                     <td>8-000-200-11-00</td>
-                    <td/>
-                    <td/>
+                    <td className={styles.btn__style}/>
+                    <td className={styles.btn__style}/>
                 </tr>
+                </tbody>
             </table>
-
-            </thead>
         </div>
 
     );
