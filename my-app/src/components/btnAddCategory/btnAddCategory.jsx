2a7ff90888895a3167f01393cfdb4a02f4d471d1
Ekaterina Shmeleva <gutkati@mail.ru>
2024-09-10 16:57:56 +0400

    Добавить валидацию поля-Добавить категорию

--- my-app/src/components/btnAddCategory/btnAddCategory.jsx
+++ my-app/src/components/btnAddCategory/btnAddCategory.jsx
@@ -9,41 +9,51 @@ const BtnAddCategory = () => {
     const arrCategories = useSelector(state => selectAllCategories(state))
 
     const [newCategory, setNewCategory] = useState('')
+    const [message, setMessage] = useState('')
     const contacts = []
 
     const onNewCategoryChange = (e) => setNewCategory(e.target.value)
 
-
-    console.log('newCategory', arrCategories)
-
     const dispatch = useDispatch()
 
     function addCategory() {
-        dispatch(categoryAdded(newCategory, contacts))
+
+        if (arrCategories.some(el => el.category === newCategory)) {
+            setMessage(<span className={styles.span}>Такая категория уже существует</span>)
+
+        } else if (newCategory === '') {
+            setMessage(<span className={styles.span}>Заполните поле</span>)
+        } else {
+            dispatch(categoryAdded(newCategory, contacts))
+            setMessage('')
+            setNewCategory('')
+        }
     }
 
     return (
-        <div className={styles.container}>
-            <div className={styles.box__input}>
-                <input
-                    className={styles.input}
-                    type="text"
-                    name=""
-                    id=""
-                    placeholder='Добавить категорию'
-                    value={newCategory}
-                    onChange={onNewCategoryChange}
+        <>
+            <div className={styles.container}>
+                <div className={styles.box__input}>
+                    <input
+                        className={styles.input}
+                        type="text"
+                        name=""
+                        id=""
+                        placeholder='Добавить категорию'
+                        value={newCategory}
+                        onChange={onNewCategoryChange}
+                    />
+                </div>
+
+
+                <button className={styles.btn}
+                        onClick={addCategory}
+                        type='submit'
                 />
+
             </div>
-
-
-            <button className={styles.btn}
-                    onClick={addCategory}
-                    type='submit'
-            />
-
-
-        </div>
+            {message}
+        </>
     );
 };
 
