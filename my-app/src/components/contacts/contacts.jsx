1d2a86944c2b4d7bf059c58bf7665cf32a404a6f
Ekaterina Shmeleva <gutkati@mail.ru>
2024-09-12 13:48:35 +0400

    Добавить popupContacts

--- my-app/src/components/contacts/contacts.jsx
+++ my-app/src/components/contacts/contacts.jsx
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './Contacts.module.css'
 import {useSelector} from "react-redux";
 import {selectAllCategories, selectContactsId, selectCategoryById} from "../../parts/categorySlice";
 import {useParams} from "react-router-dom";
+import PopupWithForm from "../popupWithForm/popupWithForm";
+import PopupContacts from "../popupContacts/popupContacts";
 
 const Contacts = () => {
     let params = useParams()
     const {contactId} = params
 
-    console.log('contactId', contactId)
+    const [isOpenPopupAddContact, setIsOpenPopupAddContact] = useState(false)
+
     const titleCategory = useSelector(state => selectCategoryById(state, contactId))
+
     // получить массив контактов из категории
     const dataContacts = useSelector((state) => selectContactsId(state, contactId))
 
     console.log('dataContacts', dataContacts)
-
-
-    console.log('dataContacts2', dataContacts.contacts)
+    console.log('titleCategory', titleCategory)
 
     const cellTable = dataContacts.contacts.map(contact => (
         <tr key={contact.id} className={styles.cell}>
@@ -28,10 +30,26 @@ const Contacts = () => {
         </tr>
     ))
 
+    function handleOpenPopupContacts() {
+        setIsOpenPopupAddContact(true)
+    }
+
+    function handleClosePopup() {
+        setIsOpenPopupAddContact(false)
+    }
 
     return (
         <div className={styles.contacts}>
             <h2 className={styles.title}>{titleCategory.category}</h2>
+
+            <div className={styles.container__btn}>
+                <button
+                    className={styles.btn__add}
+                    type='submit'
+                    onClick={handleOpenPopupContacts}
+                />
+            </div>
+
 
             <table className={styles.table}>
                 <thead>
@@ -46,6 +64,13 @@ const Contacts = () => {
                 {cellTable}
                 </tbody>
             </table>
+
+
+            <PopupContacts
+                isOpen={isOpenPopupAddContact}
+                dataContacts={dataContacts}
+                onClose={handleClosePopup}
+            />
         </div>
 
     );
